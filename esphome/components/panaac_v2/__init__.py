44fe620542cbb237fab04a07e3b8b987677c4aa2
hoangminh1109 <hoangminh1109@gmail.com>
2026-07-12 18:14:16 +0700

    add APACHE 2.0 license header

--- esphome/components/panaac_v2/__init__.py
+++ esphome/components/panaac_v2/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Minh Hoang
+# Copyright 2026 Minh Hoang
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- esphome/components/panaac_v2/panaac_v2.h
+++ esphome/components/panaac_v2/panaac_v2.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 Hoang Minh
+ * Copyright 2026 Hoang Minh
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
